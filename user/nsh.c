718a9fa422ebe13ca306d88023e46a72fc947d7d
ZZZ <965868276@qq.com>
2020-11-22 21:03:50 +0800

    done lab2

--- user/nsh.c
+++ user/nsh.c
@@ -2,28 +2,29 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 #define MAXARGS 10
+#define MAXCMD 10
 #define EXEC 0
 #define REDIR 1
 #define PIPE 2
 
-typedef struct execcmd {
-    char *argv[MAXARGS];
-}execcmd;
-typedef struct redircmd {
-    char *file;
-    int model;
-    int fd;
-}redircmd;
-typedef struct pipecmd {
-    int type;
-    struct cmd *left;
-    struct cmd *right;
-}pipecmd;
+// 要执行的指令
+typedef struct cmd{
+    int type;           // 指令种类 EXEC普通指令 REDIR重定向指令 PIPE管道指令
+    char *argv[MAXARGS];// 指令参数 EXEC有效
+    struct cmd *cmd;    // 子指令 REDIR有效
+    char *file;         // 文件名 REDIR有效
+    int model;          // 打开文件方式 REDIR有效
+    int fd;             // 要关闭的句柄 REDIR有效
+    struct cmd *left;   // 管道左端指令 PIPE有效
+    struct cmd *right;  // 管道右端指令 PIPE有效
+}Cmd;
 
+// 错误信息
 void panic(char *s) {
     fprintf(2, "%s\n", s);
     exit(-1);
 }
+// fork
 int fork1(void) {
     int pid;
     pid = fork();
@@ -32,13 +33,14 @@ int fork1(void) {
     }
     return pid;
 }
+
 char whitespace[] = " \t\r\n\v";
 char symbols[] = "<|>";
 //扫描从ps到es的字符串
 //将扫描到的token的起止位置保存在q和eq
+//ps指向下一token开头或es
 //返回token的种类
-int
-gettoken(char **ps, char *es, char **q, char **eq)
+int gettoken(char **ps, char *es, char **q, char **eq)
 {
   char *s;
   int ret;
@@ -70,118 +72,184 @@ gettoken(char **ps, char *es, char **q, char **eq)
   *ps = s;
   return ret;
 }
-void showExec(execcmd *ecmd) {
-    printf("--exec--\n");
-    int argc = 0;
-    while (ecmd->argv[argc]) {
-        printf("%d  #%s#\n", argc, ecmd->argv[argc]);
-        argc++;
-    }
-    printf("-------\n");
-    return;
-}
-//构造函数 
-int type = EXEC;
-execcmd ecmd;
-redircmd rcmd[2];
-int rcmd_cnt = 0;
-void init_execcmd (void) {
-    execcmd *p = &ecmd;
-    memset(p, 0, sizeof(ecmd));
-}
-void setRedircmd (char *file, int model, int fd) {
-    if (rcmd_cnt > 1) {
-        panic("too many redirctions");
-    }
-    redircmd *p = &rcmd[rcmd_cnt];
-    memset(p, 0, sizeof(rcmd[0]));
-    
-    rcmd[rcmd_cnt].file = file;
-    rcmd[rcmd_cnt].model = model;
-    rcmd[rcmd_cnt++].fd = fd;
+
+// 申请的cmd空间
+Cmd cmds[MAXCMD];
+int cmd_cnt = 0;
+
+void init_cmd (void) {
+    Cmd *p = cmds;
+    memset(p, 0, MAXCMD*sizeof(Cmd));
 }
 
-void parseexec(char *s) {
+void setRedircmd (Cmd* cmd, Cmd* childcmd, char *file, int model, int fd) {
+    cmd->type = REDIR;
+    cmd->cmd = childcmd;
+    cmd->file = file;
+    cmd->model = model;
+    cmd->fd = fd;
+}
+
+void showCmd(Cmd* cmd) {
+    printf("--showCmd--\n");
+    if (cmd->type == EXEC) {
+        printf("exec\n");
+        int argc = 0;
+        while (cmd->argv[argc]) {
+            printf("%d  #%s#\n", argc, cmd->argv[argc]);
+            argc++;
+        }
+    }
+    else if (cmd->type == REDIR) {
+        printf("redir\n");
+        printf("file #%s#\n", cmd->file);
+        showCmd(cmd->cmd);
+    }
+    else if (cmd->type == PIPE) {
+        printf("left\n");
+        showCmd(cmd->left);
+        printf("right\n");
+        showCmd(cmd->right);
+    }
+    printf("-----------\n");
+    return;
+}
+// 设置EXECcmd 将参数填入argv
+Cmd* parseexec(char *s, Cmd* cmd) {
+    cmd->type = EXEC;
     char *p = s;
     char *ep = s + strlen(s);
     char *q = s;
     char *eq = s;
     int argc = 0;
     while (*p) {
-        gettoken(&p, ep, &q, &eq);
+        gettoken(&p, ep, &q, &eq);  // 获取一个参数
         *eq = 0;
-        ecmd.argv[argc++] = q;
+        cmd->argv[argc++] = q;
+        if(argc >= MAXARGS)
+            panic("too many args");
     }
-    ecmd.argv[argc] = 0;
-    // showExec(&ecmd);
-    return;
+    cmd->argv[argc] = 0;
+    // showCmd(cmd);
+    return cmd;
 }
-void parsedir(char *start, char *end) {
+// 检查是否有重定向 有则设置 否则设置普通cmd
+Cmd* parseredir(char *start, char *end, Cmd *cmd) {
     char *p = start;
     char *q = start;
     char *eq = start; 
     while (*p) {
         if (*p == '<') {
             *p++ = 0;
-            if (rcmd_cnt == 0)
-                parseexec(start);
+            if (cmd->cmd != 0) { // 第二次重定向
+                //选择一个空的cmd并设置之前的重定向cmd为其子cmd
+                // printf("second redir\n");
+                gettoken(&p, end, &q, &eq); //读取文件名
+                *eq = 0;
+                setRedircmd(&cmds[cmd_cnt], cmd, q, O_RDONLY, 0);
+                // showCmd(&cmds[cmd_cnt]);
+                cmd_cnt++;
+                return &cmds[cmd_cnt-1];
+            }
+            // 第一次重定向
+            // 选取一个空的cmd作为执行指令的cmd，并设置其为子cmd
+            // printf("first redir\n");
+            parseexec(start, &cmds[cmd_cnt]); 
             gettoken(&p, end, &q, &eq); //读取文件名
-            if (!strchr(whitespace, *eq)) panic("syntax");
             *eq = 0;
-            setRedircmd(q, O_RDONLY, 0);
-            type = REDIR;
+            setRedircmd(cmd, &cmds[cmd_cnt], q, O_RDONLY, 0);
+            // showCmd(cmd);
+            cmd_cnt++;
         }
         else if (*p == '>') {
             *p++ = 0;
-            if (rcmd_cnt == 0)
-                parseexec(start);
-            gettoken(&p, end, &q, &eq);
-            if (!strchr(whitespace, *eq)) panic("syntax");
+            if (cmd->cmd != 0) { // 第二次重定向
+            //选择一个空的cmd并设置之前的重定向cmd为其子cmd
+                // printf("second redir\n");
+                gettoken(&p, end, &q, &eq); //读取文件名
+                *eq = 0;
+                setRedircmd(&cmds[cmd_cnt], cmd, q, O_CREATE | O_WRONLY, 1); 
+                // showCmd(cmd);
+                cmd_cnt++;
+                return &cmds[cmd_cnt-1];
+            }
+            // printf("first redir\n");
+            // 第一次重定向
+            // 选取一个空的cmd作为执行指令的cmd，并设置其为子cmd
+            parseexec(start, &cmds[cmd_cnt]);
+            gettoken(&p, end, &q, &eq); //读取文件名
             *eq = 0;
-            setRedircmd(q, O_CREATE | O_WRONLY, 1);
-            type = REDIR;
+            setRedircmd(cmd, &cmds[cmd_cnt], q, O_CREATE | O_WRONLY, 1);
+            cmd_cnt++;
+            // showCmd(cmd);
         }
         else p++;
     }
-    if (type == EXEC) parseexec(start);
-    return;
+    // 无重定向则设置cmd为普通cmd
+    if (cmd->cmd == 0)
+        parseexec(start, cmd);
+    return cmd;
 }
-void parsepipe(char *start, char *end) {
-
-}
-void parsecmd(char *s) {
-    char *es = s + strlen(s);
-    char *p = s;
+// 检查是否有管道 有则设置 否则检查是否有重定向
+Cmd *parsecmd(char *start, char *end) {
+    init_cmd();
+    char *p = start;
+    Cmd *cmd = &cmds[cmd_cnt++];
     while (*p) {
-        if (*p == '|') {
-            parsepipe(s, es);
-            type = PIPE;
+        if (*p == '|') { // 有管道
+            *p = 0;
+            cmd->type = PIPE;
+            cmd->left = &cmds[cmd_cnt++];
+            cmd->right = &cmds[cmd_cnt++];
+            parseredir(start, p, cmd->left);
+            parseredir(p+1, end, cmd->right);
+            // showCmd(cmd);
+            return cmd;
         }
         p++;
     }
-    parsedir(s, es);
-    return;
+    return parseredir(start, end, cmd);;
 }
-void runRedir(int cnt) {
-    close(rcmd[cnt].fd);
-    if(open(rcmd[cnt].file, rcmd[cnt].model) < 0){
-      panic("open file failed");
-    }
-    if (cnt == 1) runRedir(0);
-    exec(ecmd.argv[0], ecmd.argv);
-    return;
-}
-void runcmd(void) {
-    switch (type) {
+
+void runcmd(Cmd *cmd) {
+    int p[2];
+    switch (cmd->type) {
         case EXEC:
-            // printf("run exec\n------\n");
-            exec(ecmd.argv[0], ecmd.argv);
+            // fprintf(2, "run exec\n------\n");
+            if (cmd->argv[0] == 0) exit(-1);
+            exec(cmd->argv[0], cmd->argv);
+            fprintf(2, "exec %s failed\n", cmd->argv[0]);
             break;
         case REDIR:
-            // printf("run redir\n------\n");
-            runRedir(--rcmd_cnt);
+            // fprintf(2, "run redir\n------\n");
+            close(cmd->fd);
+            if(open(cmd->file, cmd->model) < 0)
+                panic("open file failed");
+            runcmd(cmd->cmd);
             break;
-
+        case PIPE:
+            if(pipe(p) < 0)
+                panic("pipe");
+            // printf("run pipe\n------\n");
+            if(fork1() == 0){
+                close(1);
+                dup(p[1]);
+                close(p[0]);
+                close(p[1]);
+                runcmd(cmd->left);
+            }
+            if(fork1() == 0){
+                close(0);
+                dup(p[0]);
+                close(p[0]);
+                close(p[1]);
+                runcmd(cmd->right);
+            }
+            close(p[0]);
+            close(p[1]);
+            wait(0);
+            wait(0);
+            break;
     }
     // printf("------\n");
 }
@@ -195,14 +263,22 @@ int getcmd(char *buf, int nbuf) {
 }
 
 int main(void) {
-    static char buf[100];
-
+    static char buf[10001];
+    char *p = buf;
+    char *ep = buf;
     while (getcmd(buf, sizeof(buf)) >= 0) {
-        if (fork1() == 0) {
-            parsecmd(buf);
-            runcmd();
+        p = buf;
+        while (*p) {
+            ep = p;
+            // 将buf在换行符处分开，每次执行一行
+            while (*ep && *ep != '\n') ep++;    
+            *ep = 0;
+            if (fork1() == 0) {
+                runcmd(parsecmd(p, ep));
+            }
+            wait(0);
+            p = ep + 1;
         }
-        wait(0);
     }
     exit(0);
 }
